ed918e0e75ddf05d95a511babf697baf7aa34e0c
Brännlund Luka TTK22SPO <lukabrannlund@kamk.fi>
2025-04-16 13:16:00 +0300

    sdhgoisdjfioja

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,121 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <windows.h>
+
+//textures
+sf::Texture textureAvain("textures/avain.png");
+sf::Texture texturePelaaja("textures/pelaaja.png");
+sf::Texture texturePalikka("textures/palikka.png");
+
+//sprites
+sf::Sprite spriteAvain(textureAvain);
+
+sf::Sprite spritePelaaja(texturePelaaja);
+
+sf::Sprite spritePalikkat[900];
+
+//settings
+float playerSpeed = 1;
+int testLevel[] = {
+    1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1,
+    1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1,
+};
+
+//gamestate
+float playerXPosition;
+float playerYPosition;
+
+
+void MovePlayer() {
+
+    float verticalInput = 0.0;
+    float horizontalInput = 0.0;
+
+    if (GetAsyncKeyState('W') & 0x8000) {
+        verticalInput -= 1.0 * playerSpeed;
+    }
+
+    if (GetAsyncKeyState('S') & 0x8000)
+    {
+        verticalInput += 1.0 * playerSpeed;
+    }
+
+    if (GetAsyncKeyState('D') & 0x8000) {
+        horizontalInput += 1.0 * playerSpeed;
+    }
+
+    if (GetAsyncKeyState('A') & 0x8000)
+    {
+        horizontalInput -= 1.0 * playerSpeed;
+    }
+
+    //todo collision check
+    std::cout << horizontalInput << "\n";
+    std::cout << verticalInput << "\n";
+
+    playerXPosition = playerXPosition + horizontalInput;
+    playerYPosition = playerYPosition + verticalInput;
+    
+}
+
+void InitializeLevel() {
+
+    int i = 0;
+    for (size_t y = 0; y < 30; y++)
+    {
+        for (size_t x = 0; x < 30; x++)
+        {
+            if (testLevel[i] == 0) {
+                sf::Sprite sprite(texturePalikka);
+                spritePalikkat[i] = sprite;
+                sf::Vector2f blockPosition(x * 32, -y * 32);
+                sprite.setPosition(blockPosition);
+            }
+
+            sf::Vector2f blockPosition(x * 32, -y * 32);
+            i++;
+        }
+    }
+}
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+
+    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CMake SFML Project");
+    sf::Window* pWindow = &window;
     window.setFramerateLimit(144);
 
     while (window.isOpen())
@@ -16,6 +129,21 @@ int main()
         }
 
         window.clear();
+
+        MovePlayer();
+
+        //draw level
+        for (size_t i = 0; i < 900; i++)
+        {
+            window.
+        }
+
+        //draw player
+        sf::Vector2f playerPosition(playerXPosition, playerYPosition);
+        spritePelaaja.setPosition(playerPosition);
+        window.draw(spritePelaaja);
+        
+
         window.display();
     }
 }
